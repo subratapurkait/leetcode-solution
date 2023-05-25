feed5406702d84b98ed5b91231a104d58e5367ea
Subrata Purkait <shubropurkait6@gmail.com>
2023-05-25 16:49:41 +0530

    Create lc_1.js
    1st leetcode

---
+++ lc_1.js
@@ -0,0 +1,49 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+ */
+
+
+const nums = [2,7,11,15];
+const target = 18;
+
+var twoSum = function(nums, target) {
+    const map = new Map();
+    for(let i=0; i<nums.length; i++){
+//         console.log("map", i)
+        const complement = target - nums[i];
+        
+//         console.log(map.has(complement))
+        if(map.has(complement))
+            return [map.get(complement), i] + "h";
+           
+        console.log(nums[i], i)
+        map.set(nums[i], i);
+//     console.log(map, "all")
+    }
+    return [];
+};
+
+console.log(twoSum(nums, target))
